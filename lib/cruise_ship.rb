9b19fc977651e270d4cdf04ee3b37f7bbf22a4a0
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-08 01:31:20 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,12 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
-  winner = ""
+ winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
+ 
 winner
 end
